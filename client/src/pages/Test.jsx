74948af428b9dc482cb50c7aa5576949c4f57eb7
Ayush Bunkar <ayushbunkar100@gmail.com>
2025-07-30 14:51:28 +0530

    adfs

--- client/src/pages/Test.jsx
+++ client/src/pages/Test.jsx
@@ -2,55 +2,34 @@ import { useState } from "react";
 
 const Test = () => {
   const [coords, setCoords] = useState({});
-  const handleGetLocation = () => {
+  const goToTurfWithLocation = () => {
     if ("geolocation" in navigator) {
       navigator.geolocation.getCurrentPosition(
         (position) => {
           const { latitude, longitude } = position.coords;
-          setCoords({ latitude, longitude });
+          const Tolat = 23.287404;
+          const Tolon = 77.326699;
+          window.location.href = `https://www.google.com/maps/dir/?api=1&origin=${latitude},${longitude}&destination=${Tolat},${Tolon}`;
         },
         (error) => {
-          console.error("Error fetching location:", error.message);
+          alert("Error fetching location: " + error.message);
         }
       );
     } else {
-      console.error("Geolocation is not supported by this browser.");
+      alert("Geolocation is not supported by this browser.");
     }
-  };
-
-  const getGoTurfLocation = () => {
-    handleGetLocation();
-
-    const Tolat =  23.287404;
-    const Tolon =  77.326699;
-    const FromLat = coords.latitude;
-    const FromLon = coords.longitude;
-    
-
-    console.log("Latitude:", FromLat, Tolat);
-    console.log("Longitude:", FromLon, Tolon);
-
-    window.location.href = `https://www.google.com/maps/dir/?api=1&origin=${FromLat},${FromLon}&destination=${Tolat},${Tolon}`;
-
   };
 
   return (
     <div className="p-5">
       <button
         className="border p-2 rounded mt-20"
-        onClick={handleGetLocation}
-      >
-        Get my location
-      </button>
-
-      <button
-        className="border p-2 rounded mt-20"
-        onClick={getGoTurfLocation}
+        onClick={goToTurfWithLocation}
       >
         Go turf
       </button>
     </div>
   );
 };
-
+// adfs
 export default Test;
